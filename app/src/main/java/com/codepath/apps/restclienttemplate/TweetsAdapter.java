eb1c7be0522d8287b1891ed9464c9088dda4259d
Isabel Baez <isabelbaez@fb.com>
2021-07-01 02:53:54 -0400

    Implements Home Timeline.

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -0,0 +1,126 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.content.Context;
+import android.icu.text.SimpleDateFormat;
+import android.os.Build;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import java.text.ParseException;
+import java.util.List;
+import java.util.Locale;
+
+
+public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder> {
+
+    Context context;
+    List<Tweet> tweets;
+
+    private static final int SECOND_MILLIS = 1000;
+    private static final int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+    private static final int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+    private static final int DAY_MILLIS = 24 * HOUR_MILLIS;
+
+    //pass in context and list of tweets
+
+    public TweetsAdapter(Context context, List<Tweet> tweets) {
+        this.context = context;
+        this.tweets = tweets;
+    }
+
+    //inflate a layout for each row
+
+    ImageView ivProfileImage;
+    TextView tvScreenName;
+    TextView tvBody;
+    TextView tvRelativeTime;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_tweet, parent, false);
+        return new ViewHolder(view);
+    }
+
+    //bind values based on position of element
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        //Data at position
+        Tweet tweet = tweets.get(position);
+        //Find tweet with ViewHolder
+        holder.bind(tweet);
+    }
+
+    //define a ViewHolder
+    @Override
+    public int getItemCount() {
+        return tweets.size();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public String getRelativeTimeAgo(String rawJsonDate) {
+        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+        sf.setLenient(true);
+
+        try {
+            long time = sf.parse(rawJsonDate).getTime();
+            long now = System.currentTimeMillis();
+
+            final long diff = now - time;
+            if (diff < MINUTE_MILLIS) {
+                return "just now";
+            } else if (diff < 2 * MINUTE_MILLIS) {
+                return "a minute ago";
+            } else if (diff < 50 * MINUTE_MILLIS) {
+                return diff / MINUTE_MILLIS + " m";
+            } else if (diff < 90 * MINUTE_MILLIS) {
+                return "an hour ago";
+            } else if (diff < 24 * HOUR_MILLIS) {
+                return diff / HOUR_MILLIS + " h";
+            } else if (diff < 48 * HOUR_MILLIS) {
+                return "yesterday";
+            } else {
+                return diff / DAY_MILLIS + " d";
+            }
+        } catch (ParseException e) {
+            Log.i("Tweet", "getRelativeTimeAgo failed");
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            ivProfileImage = itemView.findViewById(R.id.ivProfileImage);
+            tvScreenName = itemView.findViewById(R.id.tvScreenName);
+            tvBody = itemView.findViewById(R.id.tvBody);
+            tvRelativeTime = itemView.findViewById(R.id.tvRelativeTime);
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.N)
+        public void bind(Tweet tweet) {
+            tvBody.setText(tweet.body);
+            tvScreenName.setText(tweet.user.screenName);
+            tvRelativeTime.setText(getRelativeTimeAgo(tweet.createdAt));
+            Glide.with(context)
+                    .load(tweet.user.profileImageUrl)
+                    .into(ivProfileImage);
+        }
+    }
+}
